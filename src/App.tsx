36ea21a64bc54eb2953f33ec700ad2a7c1d108e3
aitor-iar <aarraras@iar-soft.com>
2026-02-04 15:41:17 +0100

    feat: prueba de sistema de calidad

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
     <div className="flex h-screen flex-col items-center justify-center gap-4 bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-500">Test de Calidad</h1>
       <p>Contador: {count}</p>
-      <Button onClick={() => setCount(count + 1)} variant="destructive">
+      <Button onClick={() => setCount(count + 2)} variant="destructive">
         Haz click aquí
       </Button>
     </div>
